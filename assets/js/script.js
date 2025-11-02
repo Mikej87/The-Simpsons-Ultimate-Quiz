43b8d96a2a43c92094adc56543a6fbde48038ed0
Mikej87 <michaeljib87@gmail.com>
2025-11-02 22:53:20 +0000

    First commit added index.html

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+const quizData = [{
+    
+        question: "What is the name of the bartender in Moe's Tavern?",
+        options: ["Barney", "Moe", "Lenny", "Mohammed"],
+        correctAnswer: 1
+
+        question: "What is the home address of the Simpson family?",
+        options: ["742 Evergreen Terrace", "123 Fake Street", "31 Spooner Street", "99 Maple Avenue"],
+        correctAnswer: 0
+
+        question: "Which character is known for the catchphrase 'D'oh!'?",
+        options: ["Bart Simpson", "Homer Simpson", "Ned Flanders", "Mr. Burns"],
+        correctAnswer: 1
+    }]
